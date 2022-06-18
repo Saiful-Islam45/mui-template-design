371d2e6660d614e87090796faf9e73c00b7e6888
Saiful-Islam45 <saiful.islam.ice.45@gmail.com>
2022-06-18 21:38:54 +0600

    menubar design done

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,16 +1,90 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import { Home, Person, Note, Storefront, Settings, AccountBox, ModeNight } from "@mui/icons-material";
+import GroupsIcon from '@mui/icons-material/Groups';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
+import React from "react";
 
-export const MenuBar =() => {
-    return (
-        <Box 
-            flex={1}
-            bgcolor='skyblue'
-            p={2}
-            sx={{display:{xs:'none', sm:'block'}}}
-        >
-            <div>Menubar</div>
-        </Box>
-    )
-}
+export const MenuBar = () => {
+  return (
+    <Box
+      flex={1}
+      p={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <Note />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <GroupsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <AccountBox />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon sx={{display:'flex', gap:2, alignItems:'center '}}>
+              <ModeNight />
+              <Switch/>
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
 export default MenuBar;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,66 +1,99 @@
-import React from 'react';
-import { AppBar, Avatar, Badge, Box, InputBase, styled, Toolbar, Typography } from '@mui/material';
-import CoPresentIcon from '@mui/icons-material/CoPresent';
-import { Mail, Notifications } from '@mui/icons-material';
-const profileImg = require('../img/profile.jpg')
+import React, { useState } from "react";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import CoPresentIcon from "@mui/icons-material/CoPresent";
+import { Mail, Notifications } from "@mui/icons-material";
+const profileImg = require("../img/profile.jpg");
 
-const StyleToolbar= styled(Toolbar)({
+const StyleToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  borderRadius: theme.shape.borderRadius,
+  padding: "0 20px",
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  // backgroundColor: 'white',
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
     display: "flex",
-    justifyContent:"space-between"
-})
+  },
+}));
 
-const Search= styled("div")(({theme})=>({
-    backgroundColor: 'white',
-    borderRadius: theme.shape.borderRadius,
-    padding: "0 20px",
-    width: '40%'
-}))
+const UserBox = styled(Box)(({ theme }) => ({
+  // backgroundColor: 'white',
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 
-const Icons= styled(Box)(({theme})=>({
-    // backgroundColor: 'white',
-    display: 'none',
-    gap: '20px',
-    alignItems: 'center',
-    [theme.breakpoints.up("sm")] :{
-        display: 'flex'
-    }
-}))
-
-
-const UserBox= styled(Box)(({theme})=>({
-    // backgroundColor: 'white',
-    display: 'flex',
-    gap: '10px',
-    alignItems: 'center',
-    [theme.breakpoints.up("sm")] :{
-        display: 'none',
-    }
-}))
-
-
-const Navbar =() => {
-    return (
-        <AppBar position='sticky'>
-           <StyleToolbar>
-            <Typography variant='h6' sx={{display:{xs:'none', sm:'block'}}}>Social Connect</Typography>
-            <CoPresentIcon sx={{display:{xs:'block', sm:'none'}}}/>
-            <Search><InputBase placeholder='Search...'/></Search>
-            <Icons>
-                <Badge badgeContent={9} color='error'>
-                    <Mail/>
-                </Badge>
-                <Badge badgeContent={3} color='error'>
-                    <Notifications/>
-                </Badge>
-                <Avatar sx={{width:30, height:30}} src={profileImg}/>
-            </Icons>
-            <UserBox>
-                <Avatar sx={{width:30, height:30}} src={profileImg}/>
-                <Typography variant='h6'>Saiful</Typography>
-            </UserBox>
-           </StyleToolbar>
-        </AppBar>
-    )
-}
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  return (
+    <AppBar position="sticky">
+      <StyleToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Social Connect
+        </Typography>
+        <CoPresentIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={9} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={3} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar onClick= {() => setIsOpen(!isOpen)} sx={{ width: 30, height: 30, cursor: 'pointer' }} src={profileImg} />
+        </Icons>
+        <UserBox>
+          <Avatar onClick= {() => setIsOpen(!isOpen)} sx={{ width: 30, height: 30, cursor: 'pointer' }} src={profileImg} />
+          <Typography variant="h6">Saiful</Typography>
+        </UserBox>
+        <Menu
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+        //   anchorEl={anchorEl}
+          open={isOpen}
+          onClose={() => setIsOpen(false)}  
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem>Profile</MenuItem>
+          <MenuItem>My account</MenuItem>
+          <MenuItem>Logout</MenuItem>
+        </Menu>
+      </StyleToolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
