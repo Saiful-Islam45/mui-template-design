ab8af1554303bda22120197889881fda37cffed5
saiful6356 <md.saiful.doctorkoi@gmail.com>
2022-06-22 23:59:57 +0600

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './components';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode
+    }
+  })
   return (
-    <HomePage/>
+    <ThemeProvider theme={darkTheme}>
+      <HomePage/>
+    </ThemeProvider>
   );
 }
 
